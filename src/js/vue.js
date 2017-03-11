fde063a175d8cf3ddfdb1b6a6de75baf5d1e3175
Boris Kubiak <kubiakboris@gmail.com>
2017-03-12 00:09:33 +0100

    Add link for json result

--- src/js/vue.js
+++ src/js/vue.js
@@ -2,36 +2,52 @@ new Vue({
     el: '#app',
     data: {
         badges: [],
+        json: {url: '', result: ''},
         extensions: {},
         extensionName: '',
         validName: false,
         methods: ["total", "last-version", "week", "day"],
+        status: 'loading',
     },
     methods: {
         updateBadges: function () {
             this.badges = [];
             for (let method of this.methods) {
                 let imgUrl = serverUrl + '/' + this.extensionName + '/' + method + '.svg';
-
                 this.badges.push({
                     method,
                     imgUrl,
                 });
             }
+            this.json = {
+                url: serverUrl + '/' + this.extensionName + '/stats.json',
+                result: ''
+            };
         },
 
         updateName: function () {
+            // Retrieved with jQuery as Vue.js cannot get it right in some cases including autocomplete
             this.extensionName = $('#extension-name-input').val();
             this.validName = this.extensions.hasOwnProperty(this.extensionName);
             if (this.validName) {
                 this.updateBadges();
             }
+        },
+
+        getJsonStats: function () {
+            var self = this;
+            $.ajax({
+                url: this.json.url,
+                method: 'GET',
+                dataType: 'json',
+                success: function (data) {
+                    self.json.result = JSON.stringify(data, null, 2);
+                }
+            });
         }
     },
     created: function () {
         var self = this;
-        $('.badges').removeClass('hide');
-
         var clipboard = new Clipboard('.clipboard');
 
         clipboard.on('success', function (e) {
@@ -48,22 +64,20 @@ new Vue({
             method: 'GET',
             dataType: 'json',
             success: function (data) {
-                $('.indeterminate').remove();
-                $('.progress').addClass('white').removeClass('grey');
                 Object.keys(data).forEach(function (key) {
                     data[key] = null
                 });
                 $('input.autocomplete').autocomplete({
                     data,
-                    onAutocomplete: function (t) {
+                    onAutocomplete: function () {
                         self.updateName();
-                    },
-                    // limit: 5,
+                    }
                 });
                 self.extensions = data;
+                self.status = 'loaded';
             },
             error: function () {
-                $('.server-error').removeClass('hide');
+                self.status = 'error';
             }
 
         });
